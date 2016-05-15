8d61fe1103082e50861c2cd887e7778a17ea879c
josh hieb <joshwhieb@gmail.com>
2016-05-15 17:44:00 -0600

    just put all the code into a function

--- LogFormatter.ps1
+++ LogFormatter.ps1
@@ -1,49 +1,9 @@
 ﻿<#
 #>
-function LogFormatter
+function LogFormatter($data, $leveltype)
 {
-    $obj = New-Object PSObject -Property @{        
-    };
-    
-    $obj | Add-Member -MemberType ScriptMethod -Name getDate -Value {
-    
-        $dateString = Get-Date -format dd/MM/yyyy;
-        $timeString = Get-Date -format HH:mm:ss;     
-        return $dateString,$timeString
-    };       
-    
-    $obj | Add-Member -MemberType ScriptMethod -Name FormatData -Value {
-        
-        param($data, $levelType)
+    $dateString = Get-Date -format dd/MM/yyyy;
+    $timeString = Get-Date -format HH:mm:ss;     
 
-        $date, $time = $this.getDate()
-
-        return "$date $time [$levelType]: $data"
-
-
-    };
-
-    $obj | Add-Member -MemberType ScriptMethod -Name log -Value {
-    
-        param($data,$levelType);
-        
-        $dataToLog = $this.FormatData($data,$levelType);       
-		return $dataToLog    
-    };
-    
-    <#
-        Function: Destroy
-        Purpose: Destructor for the class
-            
-        Params: none
-        Returns: none
-    #>
-    $obj | Add-Member -MemberType ScriptMethod -Name Destroy -Value {
-                
-    };
-        
-    <#
-        Return object closure
-    #>
-    return $obj;
-}
+    return "$dateString $timeString [$levelType]: $data"
+}LogFormatter -data "hello" -leveltype info
